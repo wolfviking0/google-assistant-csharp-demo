c7bd46a150520baee7afa635a6505dd5244d6131
Anthony Liot <anthony.liot@gmail.com>
2018-11-30 18:57:20 -0800

    Initialize csharp project
    Signed-off-by: Anthony Liot <anthony.liot@gmail.com>

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace googleassistantcsharpdemo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
